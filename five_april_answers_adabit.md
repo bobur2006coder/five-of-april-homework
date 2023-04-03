3e5c99e1f8409082d7bbcb469fb2075fb581cf9c
bobur2006coder <boburdilmurodov702@gmail.com>
2023-04-03 17:00:36 +0500

    edabit

---
+++ five_april_answers_adabit.md
@@ -0,0 +1,22 @@
+<!-- (one answer) -->
+
+function addition(a, b) {
+	return a+b;
+}
+
+
+
+<!-- (two answer) -->
+
+function convert(minutes) {
+	return (minutes*60);
+}
+
+
+
+
+
+
+
+
+
---
+++ js.js
@@ -0,0 +1,25 @@
+// 1-ishlanishi
+
+
+// let a = + prompt("soatni kiriting")
+// let b = + prompt("ikkinchi sonni kiriting")
+
+// function addition(a, b) {
+// 	alert(a+b) ;
+    
+// }
+
+// addition(a,b)
+
+
+// 2-ishlanishi
+
+// let a = + prompt("soatni kiriting")
+
+// function vaqt(a) {
+//     alert(a*60);
+    
+// }
+
+// vaqt(a);
+
